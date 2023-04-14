45cd882d370e13819ec70829c13dc6fd6b13c1ce
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-14 18:29:57 +0300

    Scaled the data, decided to use the silhouette method to choose the number of my clusters.

--- main.py
+++ main.py
@@ -10,4 +10,19 @@ X = iris.data
 
 #  Load the data into a pandas dataframe
 df = pd.DataFrame(X, columns=iris.feature_names)
-print(df.columns)
+# print(df.columns)
+
+#  Scale the dataframe
+scaler = StandardScaler()
+scaler.fit(df)
+scaled_data = scaler.transform(df)
+# print(scaled_data)
+
+# choose the number of clusters to create using the elbow method
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++',
+                    max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(scaled_data)
+    wcss.append(kmeans.inertia_)
+print(wcss)
